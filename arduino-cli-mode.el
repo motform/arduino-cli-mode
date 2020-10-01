fd77ecbefdade2e4ca40ce811d7053b3851f9860
motform <love.lagerkvist@protonmail.com>
2020-10-01 15:57:07 +0200

    Update commentary to match README

--- arduino-cli-mode.el
+++ arduino-cli-mode.el
@@ -28,18 +28,20 @@
 
 ;;; Commentary:
 
-;; This package contains a wrapper for the official Arduino command line utility.
-;; It aims to be as simple as possible, while still providing useful conveniences.
+;; arduino-cli-mode is an Emacs minor mode for using the excellent new
+;; Arduino command line interface in an Emacs-native fashion.  The mode
+;; covers the full range of arduino-cli features in an Emacs native
+;; fashion.  The mode leverages the infinite power the GNU to provide
+;; fussy-finding of libraries and much improved support for handling
+;; multiple boards.  The commands that originally require multiple
+;; steps (such as first searching for a library and then separately
+;; installing it) have been folded into one.
+;; 
+;; For more information on the package and default board behavior,
+;; see the README at https://github.com/motform/arduino-cli-mode
 ;;
-;; The structure of this package was inspired by ZachMassia's PlatformIO-Mode:
-;; https://github.com/ZachMassia/PlatformIO-Mode/
-;; 
-;; Most of the fns consist of hairy imperative let*s that check/get
-;; something from the cli.  Mostly parsing json or getting things from
-;; maps that are structured in semi-coherent ways.
-;; 
-;; For more information on the wrapper, see the readme at https://github.com/motform/arduino-cli-mode
-;; For more information on arduino-cli itself, see https://github.com/arduino/arduino-cli
+;; For more information on arduino-cli itself,
+;; see https://github.com/arduino/arduino-cli
 ;;
 ;; Tested against arduino-cli >= 0.10.0
 
